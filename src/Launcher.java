b6e8a4447526a0c3a616baa88e21f92555842827
antant <antonkhvdv@gmail.com>
2016-07-15 20:42:52 +1200

    Launcher.java modified

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,9 +1,6 @@
 
 public class Launcher {
-
 	public static void main(String[] args) {
-		System.out.print("hello git , im Iggy <3");
-
+		System.out.print("hello git testtesttest , im Iggy <3");
 	}
-
 }
