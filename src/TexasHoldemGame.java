69b285445a979d675e8964df7fe93b71b8a24a66
iggytoto <iggytoto@gmail.com>
2016-07-16 13:43:09 +0300

    Table settings in menu

--- src/TexasHoldemGame.java
+++ src/TexasHoldemGame.java
@@ -1,7 +1,10 @@
+import Model.TableSettings;
+import Model.Enumerations.RaisingPolicy;
 import Utility.ConsoleHelper;
 
 public class TexasHoldemGame {
 	//private 
+	private TableSettings settings;
 	
 	public void run()
 	{
@@ -24,7 +27,7 @@ public class TexasHoldemGame {
 				endGame();
 				break;
 			case 1:
-				settingsDialog();
+				settings = settingsDialog();
 				break;
 			default:
 				System.out.println("Incorrect value entered, please try again once more...");
@@ -33,7 +36,65 @@ public class TexasHoldemGame {
 		}
 	}
 
-	private void settingsDialog() {
+	private TableSettings settingsDialog() {
+		System.out.println("Table settings...");
+
+		
+		while(true)
+		{
+			int smallBlind;
+			int playersCount;
+			int startingMoney;	
+			System.out.println("Enter number of players...");
+			while(true)
+			{
+				int value = ConsoleHelper.getInstance().readInt();
+				if(value >= 2 && value <= 8)
+				{
+					playersCount = value;
+					break;
+				}
+				else
+				{
+					System.out.println("Enter amount between 2 and 8");
+				}
+			}
+			System.out.println("Enter small blind amount...");
+			while(true)
+			{
+				int value = ConsoleHelper.getInstance().readInt();
+				if(value > 0)
+				{
+					smallBlind = value;
+					break;
+				}
+				else
+				{
+					System.out.println("Enter positive small blind amount...");
+				}
+			}
+			System.out.println("Enter starting money amount...");
+			while(true)
+			{
+				int value = ConsoleHelper.getInstance().readInt();
+				if(value > 0)
+				{
+					startingMoney = value;
+					break;
+				}
+				else
+				{
+					System.out.println("Enter positive starting money amount...");
+				}
+			}
+			ConsoleHelper.getInstance().clearConsole();
+			System.out.printf("You choosed\nPlayers:%d\nSmallBlind:%d\nStartingMoney:%d\nIs that correct?(y/n)", playersCount,smallBlind,startingMoney);
+			if(ConsoleHelper.getInstance().readYes())
+			{
+				return new TableSettings(playersCount,startingMoney,smallBlind,RaisingPolicy.NoLimits);
+			}
+			ConsoleHelper.getInstance().clearConsole();
+		}
 	}
 
 	private void endGame()
