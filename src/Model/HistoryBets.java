e41387a6f12ae7f82edbacc52c9b861e52e31a67
arhipovartem <artemarhipov@10.242.1.214>
2016-07-16 03:42:53 +0300

    //

--- src/Model/HistoryBets.java
+++ src/Model/HistoryBets.java
@@ -8,40 +8,17 @@ public class HistoryBets {
 	private final StageType type;
 	private final int idPlayer;
 	private final ActionType BetType;
+	private final int amount;
 	
-	HistoryBets(StageType type, int idPlayer, ActionType BetType)
+	HistoryBets(StageType type, int idPlayer, ActionType BetType, int amount)
 	{
 		this.type=type;
 		this.idPlayer=idPlayer;
 		this.BetType=BetType;
+		this.amount=amount;
 		
 	
 	}
 }
 
 
-/*import Model.Enumerations.CardType;
-import Model.Enumerations.CardValue;
-
-public class Card 
-{
-	//Fields
-	private final CardType type;
-	private final CardValue value;
-	//Constructor
-	public Card(CardType type,CardValue value)
-	{
-		this.type = type;
-		this.value = value;
-	}
-	//Getters
-	public CardType GetType()
-	{
-		return type;
-	}
-	
-	public CardValue GetValue()
-	{
-		return value;
-	}
-}*/
