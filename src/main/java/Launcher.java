f268b447bf1862bf2a026abd11d88c8a767d5159
iggytoto <iggytoto@gmail.com>
2016-07-18 23:54:38 +0300

    Personal branch created

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -3,6 +3,6 @@ package main.java;
 public class Launcher {
 	public static void main(String[] args) {
 		TexasHoldemGame game = new TexasHoldemGame();
-		game.run();
+		game.run(); //game runs
 	}
 }
