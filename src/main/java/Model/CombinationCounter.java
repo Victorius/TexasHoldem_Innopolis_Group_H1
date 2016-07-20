4beab617b48618a5e44cbf089d499f7e31b250e0
Victorius <ricka_elf@mail.ru>
2016-07-20 19:31:58 +0400

    Refactoring
    Refactoring in CounterCombination

--- src/main/java/Model/CombinationCounter.java
+++ src/main/java/Model/CombinationCounter.java
@@ -21,41 +21,13 @@ public abstract class CombinationCounter {
 			return new Combination(CombinationType.NoCombination,new ArrayList<Card>());
 		Collections.sort(allCards);
 		ArrayList<ArrayList<Card>> pairs = new ArrayList<ArrayList<Card>>();
-//		ArrayList<Card> toStraight = new ArrayList<Card>();		
-//		ArrayList<Card> toFlash = new ArrayList<Card>();
 		int sp=0;
-//		int pointerOfStraight = 0;
 		boolean isFlash =false;
 		//we calculate flash. 
 		//if one the three cards has a equal CardType 4 another card 
 		Combination flash = isFlash(allCards);
 		if(flash!=null)
 			isFlash =true;
-//		int count2straight=0;
-//		for(int i=allCards.size()-1;i>allCards.size()-3;i--){
-//			count2straight=i;
-//			while(count2straight>0 && allCards.get(count2straight).getValue().getValue()==allCards.get(count2straight-1).getValue().getValue()-1){
-//				pointerOfStraight++;
-//				toStraight.add(allCards.get(count2straight--));			
-//			}
-//			if(pointerOfStraight>=5)
-//				break;
-//		}		
-		//We define which of the cards take a part in the straight flash
-//		if(pointerOfStraight>=5){
-//			toStraight.add(0,allCards.get(count2straight));			
-//			boolean straightFlash=true;
-//			for(int j=1;j>=0;j--){
-//				for(int i=j;i<j+5;i++)
-//					straightFlash&=toStraight.get(i).getType().equals(toStraight.get(i+1).getType());
-//				if(straightFlash){
-//					toStraight = (ArrayList<Card>) toStraight.subList(pointerOfStraight-SIZE_OF_COMBINATION-j-1, toStraight.size()-j-1);
-//					return new Combination(CombinationType.StraightFlash,toStraight);
-//				}
-//			}
-//			toStraight = (ArrayList<Card>) toStraight.subList(pointerOfStraight-5, toStraight.size());
-//		}
-		
 		do{
 			int count=0;
 			ArrayList<Card> cards = new ArrayList<Card>();			
@@ -82,9 +54,6 @@ public abstract class CombinationCounter {
 			case 2:
 				for(int j=0;j<pairs.size();j++){
 					if(i!=j && pairs.get(j).size()==2){
-						
-//						if(pointerOfStraight>=5)
-//							return new Combination(CombinationType.Straight,toStraight);
 						if(isFlash)
 							return flash;
 						if(isStraight)
@@ -146,10 +115,7 @@ public abstract class CombinationCounter {
 					return flash;
 				if(isStraight)
 					return straight;
-							
-//				if(pointerOfStraight>=SIZE_OF_COMBINATION)
-//					return new Combination(CombinationType.Straight,toStraight);
-				
+			
 				for(int countCard=0;countCard<pairs.get(i).size();countCard++)
 					allCards.remove(pairs.get(i).get(countCard));
 				pair = new ArrayList<Card>();
@@ -177,15 +143,7 @@ public abstract class CombinationCounter {
 				resultWithHighCard.add(allCards.get(i));
 		return new Combination(CombinationType.HighCard,resultWithHighCard);
 	}
-	private ArrayList<Card> findTheSameCard(Card card,List<Card> source){
-		ArrayList<Card> result = new ArrayList<Card>();
-		for(Card i: source){
-			if(i.getValue().getValue()==card.getValue().getValue()){
-				result.add(i);
-			}
-		}
-		return result;
-	}
+	
 	public void setCards(ArrayList<Card> hand){
 		this.cards=hand;
 	}
