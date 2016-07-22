2b653c20f4ce4f3c353474cd740bfb2cba45ec73
Victorius <ricka_elf@mail.ru>
2016-07-22 10:42:14 +0400

    Added test random AI
    Added test random AI check/call/raise

--- src/tests/java/AIBluffStrategyTest.java
+++ src/tests/java/AIBluffStrategyTest.java
@@ -9,9 +9,11 @@ import org.junit.Test;
 
 import AiStrategies.BluffStrategy;
 import main.java.Model.AiPlayer;
+import main.java.Model.Bet;
 import main.java.Model.Card;
 import main.java.Model.Table;
 import main.java.Model.TableSettings;
+import main.java.Model.Enumerations.ActionType;
 import main.java.Model.Enumerations.CardType;
 import main.java.Model.Enumerations.CardValue;
 import main.java.Model.Enumerations.RaisingPolicy;
@@ -37,19 +39,29 @@ public class AIBluffStrategyTest {
 		a.get(1).setCards(card2ai);
 		card2ai = new ArrayList<Card>();
 		card2ai.add(new Card(CardType.Clubs, CardValue.Six));
-		card2ai.add(new Card(CardType.Clubs, CardValue.Three));
-//		a.get(2).set
+		card2ai.add(new Card(CardType.Clubs, CardValue.Two));
+		a.get(2).setCards(card2ai);
 	    tableCard.add(new Card(CardType.Diamonds, CardValue.Three));
 	    tableCard.add(new Card(CardType.Hearts, CardValue.Five));
 	    tableCard.add(new Card(CardType.Clubs, CardValue.Jack));
 	    tableCard.add(new Card(CardType.Diamonds, CardValue.Queen));
 	    tableCard.add(new Card(CardType.Hearts, CardValue.Nine));
-		
+	    
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRandomAnswerCallCheckRandom() {
+		int circle = 0;
+		int smallBlind=40;
+		Bet c1a1 = new Bet(circle,a.get(0),smallBlind);
+		Bet c1a2 = new Bet(circle,a.get(1),smallBlind*2);
+		a.get(0).setLastBet(c1a1);
+		a.get(1).setLastBet(c1a2);
+		table.addBetToList(c1a1);
+		table.addBetToList(c1a2);
+		
+		assertEquals(true, a.get(2).getPlayerAction().getType()==ActionType.CallCheck ||  a.get(2).getPlayerAction().getType()==ActionType.Raise);
 	}
+	
 
 }
