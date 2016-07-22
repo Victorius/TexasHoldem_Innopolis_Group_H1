3d3b19dec5d32c09c27b7fa902423c4d8811c1e9
iggytoto <iggytoto@gmail.com>
2016-07-22 09:48:07 +0300

    ai fixed

--- src/AiStrategies/AiStrategy.java
+++ src/AiStrategies/AiStrategy.java
@@ -33,7 +33,7 @@ public abstract class AiStrategy {
 	
 	protected boolean canCheck(Player player, Table table) {
 		List<Bet> bets = table.getBets();
-		int lastCircle = getMaxCircle(bets);
+		int lastCircle = getMaxCircle(bets) - 1;
 		Bet max = getMaxBet(bets, lastCircle);
 		Bet last = player.getLastBet();
 		if(last == null){
